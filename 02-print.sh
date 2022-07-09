bc4464482660903828bf646685b04a916b83623e
Rahul <rahul>
2022-07-09 15:41:15 +0530

    updated 01-Sample.sh

--- 02-print.sh
+++ 02-print.sh
@@ -2,4 +2,4 @@
 
 echo -e "\e[33m Printing in Yellow Color\e[0m"
 
-echo -e "\e[44;43m Printing in Blue Color with Yellow Background\e[0m"
+echo -e "\e[44;33m Printing in Blue Color with Yellow Background\e[0m"
