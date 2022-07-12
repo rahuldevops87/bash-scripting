1c9c1b41d616487bb0d8429ecbfc08a245adfb61
Rahul <rahul>
2022-07-12 16:07:34 +0530

    frontend-script

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-echo "I am in frontend"
+REPO_URL="https://github.com/stans-robot-project/frontend/archive/main.zip"
+
+yum install nginx -y
+systemctl enable nginx
+systemctl start nginx
+curl -s -L -o /tmp/frontend.zip $REPO_URL
+cd /usr/share/nginx/html
+rm -rf *
+unzip /tmp/frontend.zip
+mv frontend-main/* .
+mv static/* .
+rm -rf frontend-main README.md
+mv localhost.conf /etc/nginx/default.d/roboshop.conf
