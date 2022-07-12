2adf2a58fb57afa645b0d9e049fa65b2f8b41366
Rahul <rahul>
2022-07-12 16:45:02 +0530

    frontend-script

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 USER_ID=$(id -u)
 
 if [ $USER_ID -ne 0 ] ; then
--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 
-# Script will run the below commands sequentially, if any command fails, script will exit by using 'set -e' option
+# Script will run the below commands sequentially
+#if any command fails, script will exit by using 'set -e' option
+
 set -e
 
 source components/common.sh
 
 COMPONENT=frontend
+LOGFILE=/tmp/$COMPONENT.log
+
 REPO_URL="https://github.com/stans-robot-project/frontend/archive/main.zip"
 
-yum install nginx -y
+echo -n "Installing NGNIX"
+yum install nginx -y &>> $LOGFILE
+
+if [ $? -eq 0] ; then
+    echo -e "\e[32mSUCCESS\e[0m"
+fi
+
 systemctl enable nginx
 systemctl start nginx
 curl -s -L -o /tmp/frontend.zip $REPO_URL
