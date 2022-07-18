a9ae5d18b1f11abd8ebcaf45ca6b21195159d8a4
Rahul <rahul>
2022-07-18 12:31:31 +0530

    AWS-instance-draft

--- AWS-instance.sh
+++ AWS-instance.sh
@@ -2,4 +2,4 @@ Image=$(aws ec2 describe-images --filters "Name=name,Values=CloudDevOps-LabImage
 
 Security=$(aws ec2 describe-security-groups --filters "Name=ip-permission.cidr,Values=0.0.0.0/0"|jq '.SecurityGroups[].GroupId'| sed -e 's/"//g')
 
-aws ec2 run-instances --image-id $Image --count 1 --instance-type t2.micro --security-group-ids $Security
+aws ec2 run-instances --image-id $Image --count 1 --instance-type t2.micro --security-group-ids $Security | jq
